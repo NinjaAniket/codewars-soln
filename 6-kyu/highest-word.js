2958d602e72b1f89452e168f063d2bb3aa0ce7a3
aniket <aniket@anikets-MacBook-Air.local>
2021-11-07 23:49:02 +0530

    highest-order

---
+++ 6-kyu/highest-word.js
@@ -0,0 +1,18 @@
+function high(char) {
+  let highestScore = 0;
+  let highestLetter = "";
+
+  for (let str of char.split(" ")) {
+    let currentLetter = 0;
+    for (let word of str) {
+      currentLetter += word.charCodeAt() - 96;
+    }
+    if (currentLetter > highestScore) {
+      highestScore = currentLetter;
+      highestLetter = str;
+    }
+  }
+  return highestLetter;
+}
+
+console.log(high("man i need a taxi up to ubud"));
