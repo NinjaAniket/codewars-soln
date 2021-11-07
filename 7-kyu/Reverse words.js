431293462f1f31d81cc906db44ff74cdc50cef36
aniket <aniket@anikets-MacBook-Air.local>
2021-11-07 22:41:34 +0530

    reverse words

---
+++ 7-kyu/Reverse words.js
@@ -0,0 +1,9 @@
+function reverseWords(str) {
+  // Go for it
+
+  return str
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
+}
+console.log(reverseWords("The quick brown fox jumps over the lazy dog."));
